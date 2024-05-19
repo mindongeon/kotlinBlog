40bfc55038ade0a142d9a63c4dbd70f35c4d62c6
omen <alsehddjs@gmail.com>
2024-05-19 18:04:38 +0900

    a

--- src/main/kotlin/org/example/simpleblog/domain/comment/Comment.kt
+++ src/main/kotlin/org/example/simpleblog/domain/comment/Comment.kt
@@ -1,4 +1,18 @@
 package org.example.simpleblog.domain.comment
 
-class Comment {
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+import org.example.simpleblog.domain.AuditingEntity
+
+@Entity
+@Table(name = "Comment")
+class Comment(
+    content: String
+) : AuditingEntity() {
+
+    @Column(name = "content", nullable = false)
+    var content: String = content
+        private set
+
 }
