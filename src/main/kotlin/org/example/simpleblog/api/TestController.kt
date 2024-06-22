4bf594d8d72378e04a9baea5521c4a066305a742
민동언 <mindongeon@mindong-eon-ui-MacBookPro.local>
2024-06-23 00:43:14 +0900

    currying function in kotlin

--- src/main/kotlin/org/example/simpleblog/api/TestController.kt
+++ src/main/kotlin/org/example/simpleblog/api/TestController.kt
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class TestController (
-
-){
+class TestController {
 
     @GetMapping("/health")
-    fun healthTest():String = "hello kotlin-blog"
+    fun healthTest(): String = "hello kotlin-blog"
+
+//    @GetMapping("/error") // spring security default error redirect
+//    fun errorTest(): String = "error"
+
 }
--- src/main/kotlin/org/example/simpleblog/config/security/CustomBasicAuthenticationFilter.kt
+++ src/main/kotlin/org/example/simpleblog/config/security/CustomBasicAuthenticationFilter.kt
@@ -24,8 +24,9 @@ class CustomBasicAuthenticationFilter(
 
     private val jwtManager = JwtManager()
 
-    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
-
+    override fun doFilterInternal(
+        request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain
+    ) {
         log.debug { "권한이나 인증이 필요한 요청이 들어옴" }
 
         val accessToken = request.getHeader(jwtManager.authorizationHeader)?.replace("Bearer ", "")
@@ -34,67 +35,76 @@ class CustomBasicAuthenticationFilter(
             chain.doFilter(request, response)
             return
         }
-
         log.debug { "access token: $accessToken" }
 
         val accessTokenResult: TokenValidResult = jwtManager.validAccessToken(accessToken)
 
         if (accessTokenResult is TokenValidResult.Failure) {
-            if (accessTokenResult.exception is TokenExpiredException) {
+
+            handleTokenException(accessTokenResult) {
                 log.info { "getClass ::: ${accessTokenResult.javaClass}" }
 
-                val refreshToken = CookieProvider.getCookie(request, CookieProvider.CookieName.REFRESH_COOKIE).orElseThrow()
+                val refreshToken =
+                    CookieProvider.getCookie(request, CookieProvider.CookieName.REFRESH_COOKIE)
+                        .orElseThrow()
                 val refreshTokenResult = jwtManager.validRefreshToken(refreshToken)
 
                 if (refreshTokenResult is TokenValidResult.Failure) {
                     // 클라이언트 단에서 받아서 강제 로그아웃 처리 등 해버리기
                     throw RuntimeException("invalid refresh token")
                 }
-                /**
-                 * 이미 발급한 refreshToken (쿠키로 감싸져 있음)
-                 * 까내서 요걸 토대로 다시 accessToken 발급하기.
-                 */
-
+                /* 이미 발급한 refreshToken (쿠키로 감싸져 있음)
+                  까내서 요걸 토대로 다시 accessToken 발급하기. */
                 val principalString = jwtManager.getPrincipalStringByRefreshToken(refreshToken)
                 val details = om.readValue(principalString, PrincipalDetails::class.java)
 
-                val accessToken = jwtManager.generateAccessToken(om.writeValueAsString(details))
-                response.addHeader(jwtManager.authorizationHeader, "${jwtManager.jwtHeader} $accessToken")
-
-                val authentication: Authentication = UsernamePasswordAuthenticationToken(
-                    details,
-                    details.password,
-                    details.authorities
-                )
-                SecurityContextHolder.getContext().authentication = authentication // 인증 처리 끝
-                chain.doFilter(request, response)
-
-                return
-            } else {
-                log.error { accessTokenResult.exception.stackTraceToString() }
+                reissueAccessToken(details, response)
+                setAuthentication(details, chain, request, response)
             }
+            return
         }
 
-
         val principalJsonData = jwtManager.getPrincipalStringByAccessToken(accessToken)
-
-
         val principalDetails = om.readValue(principalJsonData, PrincipalDetails::class.java)
 
+        setAuthentication(principalDetails, chain, request, response)
+    }
 
+    private fun setAuthentication(
+        details: PrincipalDetails,
+        chain: FilterChain,
+        request: HttpServletRequest,
+        response: HttpServletResponse
+    ) {
 //        val member = memberRepository.findMemberByEmail(details.member.email)
         // 인증객체
 //        val principalDetails = PrincipalDetails(member)
         // 인자가 2개일 경우 credentials이 암호화되기 전의 값과 비교하기 때문에 무조건 denied 된다.
         val authentication: Authentication = UsernamePasswordAuthenticationToken(
-            principalDetails,
-            principalDetails.password,
-            principalDetails.authorities
+            details, details.password, details.authorities
         )
-
-
-        SecurityContextHolder.getContext().authentication = authentication
+        SecurityContextHolder.getContext().authentication = authentication // 인증 처리 끝
         chain.doFilter(request, response)
+    }
+
+    private fun reissueAccessToken(
+        details: PrincipalDetails?, response: HttpServletResponse
+    ) {
+        log.info { "accessToken reGeneration" }
+        val accessToken = jwtManager.generateAccessToken(om.writeValueAsString(details))
+        response.addHeader(jwtManager.authorizationHeader, "${jwtManager.jwtHeader} $accessToken")
+    }
+
+    private fun handleTokenException(tokenValidResult: TokenValidResult.Failure, func: () -> Unit) {
+
+        when (tokenValidResult.exception) {
+            is TokenExpiredException -> func()
+            else -> {
+                log.error(tokenValidResult.exception.stackTraceToString())
+                throw tokenValidResult.exception
+            }
+        }
+
     }
 
 
